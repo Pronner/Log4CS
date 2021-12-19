317031b90790663ec8ce7ae31a6bb405b73f5587
Pronner <pronnerdev@gmail.com>
2021-12-19 20:17:12 +0400

    Log4CS source code
    Log4CS source code

---
+++ Log4CS/Log4CS/Log4CS.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Log4CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.terminal = new System.Windows.Forms.RichTextBox();
+            this.StartLogger = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // terminal
+            // 
+            this.terminal.BackColor = System.Drawing.Color.Black;
+            this.terminal.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.terminal.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.terminal.Location = new System.Drawing.Point(-1, 0);
+            this.terminal.Name = "terminal";
+            this.terminal.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.terminal.Size = new System.Drawing.Size(865, 491);
+            this.terminal.TabIndex = 0;
+            this.terminal.Text = "";
+            this.terminal.TextChanged += new System.EventHandler(this.terminal_TextChanged);
+            this.terminal.KeyDown += new System.Windows.Forms.KeyEventHandler(this.terminal_KeyDown);
+            // 
+            // StartLogger
+            // 
+            this.StartLogger.Interval = 1;
+            this.StartLogger.Tick += new System.EventHandler(this.StartLogger_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(860, 489);
+            this.Controls.Add(this.terminal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Log4CS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox terminal;
+        private System.Windows.Forms.Timer StartLogger;
+    }
+}
+
---
+++ Log4CS/Log4CS/Log4CS.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Log4CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void terminal_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        public string AfterReturn = "";
+
+        private void terminal_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                if (terminal.Text == "help")
+                {
+
+                    MessageBox.Show("help, logstart, path (replace 'path' with an executable path to start)", "Commands", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    terminal.Clear();
+
+                }
+                else if (terminal.Text == "logstart")
+                {
+
+                    StartLogger.Start();
+                    terminal.Clear();
+
+                }
+                else
+                {
+
+                    try
+                    {
+
+
+                        ProcessLog = terminal.Text;
+                        Process.Start(terminal.Text);
+                        terminal.Clear();
+
+                        TextWriter textw = new StreamWriter("proclogger.log4cs", false);
+                        textw.WriteLine("[" + DateTime.Now.ToString("t") + "] Started Process: " + ProcessLog);
+                        textw.Close();
+
+                    }
+                    catch
+                    {
+
+                        MessageBox.Show("There was a problem starting this program... Please try again and verify the executable's location.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        terminal.Clear();
+
+                    }
+
+                }
+
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        string ProcessLog = "";
+
+        private string ReplaceAction = "";
+
+        private void StartLogger_Tick(object sender, EventArgs e)
+        {
+
+            // Start logger
+
+            if (ReplaceAction == "") // Replace this with another if statement to log whatever you're looking to log.
+            {
+                if (File.Exists("log4.cs"))
+                {
+
+                    TextWriter textw = new StreamWriter("logs.txt", false);
+                    textw.WriteLine("Log something here.");
+                    textw.Close();
+
+                }
+            }
+
+        }
+    }
+}
