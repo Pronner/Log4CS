6f60ff7da55dd7de1a0b7439a25a7e8d4d887fc7
Pronner <pronnerdev@gmail.com>
2022-04-04 14:29:35 +0400

    Updated source code
    Updated source code

---
+++ Log4CS/Log4CS.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Log4CS;
+using Log4CS.Console;
+using Log4CS.Library;
+
+namespace Log4CS.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Log4CS/Log4CS.Library/Logger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace Log4CS.Library
+{
+    public class Logger
+    {
+
+        public string logPath = null; // unnecessary, just leaving it here tho
+        private static string txtLog;
+
+        public static void CreateLog(string path)
+        {
+
+            try
+            {
+
+                File.Create(path);
+
+            }
+            catch (IOException ioex)
+            {
+
+                Console.WriteLine(ioex);
+
+            }
+
+        }
+
+        public static void writeLog(string whatToWrite, string path)
+        {
+
+            txtLog = txtLog + whatToWrite + Environment.NewLine;
+            File.WriteAllText(path, txtLog);
+
+        }
+
+        public static void openLog(string path)
+        {
+
+
+            System.Diagnostics.Process.Start(path);
+
+
+        }
+
+    }
+}
